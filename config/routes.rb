02f5c2773b61d5eee3d58798cc394df0bdb2e1e2
Max Alberts <maxalbertsneede@gmail.com>
2017-09-08 18:43:07 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'activity/mine'
+
+  get 'activity/feed'
+
+  
+
+
+  # Root at the bottom of the file:
+  root to: 'activity#mine'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
